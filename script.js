273f9bc037edc1e2f3925e9f4a04bd17b77c29ac
Andrea Duran <69520082+durandre@users.noreply.github.com>
2020-09-24 19:57:10 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -5,13 +5,12 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  var number = [0,1,2,3,4,5,6,7,8,9
   ]
 
   passwordText.value = password;
 }
   function generatePassword() {
-    
+
   var number=prompt("How many characters would you like your password to be? choose a number between 8-128");
   console.log(number);
   var lower=confirm("Do you want to include lowercase characters");
@@ -21,6 +20,12 @@ function writePassword() {
   var numeric=confirm("Do you want your password to be numeric?");
   console.log(numeric);
   var SpecChar=confirm("Do you want your password to include special characters?");
+  var numeric = "0,1,2,3,4,5,6,7,8,9";
+  var lower = "abcdefghijklmnopqrstuvwxyz"
+  var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  var SpecChar = "!@#$%^&*()_+";
+
+
 }
 createPassword(number,string){
   var password="" 
