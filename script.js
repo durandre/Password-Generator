0e3c6d0c1057a60678c828820b71b7a154ce5ea4
Andrea Duran <aduran2099@gmail.com>
2020-11-17 11:57:26 -0600

    commit on main

--- script.js
+++ script.js
@@ -8,65 +8,69 @@ var confirmspecialChar = confirm("Click ok to confirm if you would like to inclu
 var confirmUppercase = confirm("Click ok to confirm if you would like to include uppercase characters");
 var confirmLowercase = confirm("Click ok to confirm if you would like to include lowercase characters");
 
-
-
 function generatePassword() {
 
+  enter = parseInt(prompt("How many characters would you like your password? Choose between 8 and 128"));
+
+  if (!enter) {
+    alert("This needs a value");
+  } else if (enter < 8 || enter > 128) {
+
+    enter = parseInt(prompt("You must choose between 8 and 128"));
+
+    var characters = lowercase;
+    var password = "";
+    if (uppercaseCheck && numberCheck && specialCharCheck) {
+      characters += uppercase + number + specialChar;
+
+    } else if (uppercaseCheck && numberCheck) {
+      characters += uppercase + number;
+
+    } else if (numberCheck && specialCheck) {
+      characters += number + specialChar;
+
+    } else if (uppercaseCheck && specialCheck) {
+      characters += uppercase + specialChar;
+
+    } else if (uppercaseCheck) {
+      characters += uppercase;
+
+    } else if (numberCheck) {
+      characters += number;
+
+    } else if (specialCheck) {
+      characters += specialChar;
+
+    } else {
+      characters === lowercase;
+    }
+
+    for (var i = 0; i < passwordLength; i++) {
+      password += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return password;
+
+    function writePassword() {
+      var passLength = prompt("How many characters out of 8-128 do you want your password to be?");
+      var length = passLength
+      characters = "`~!@#$%^&*()_+\*-=[];'/.,<>?:ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890",
+        passwordText = "";
 
 
-var characters = lowercase;
-var password = "";
-if (uppercaseCheck && numberCheck && specialCharCheck) {
-  characters += uppercase + number + specialChar;
+      for (var i = 0, n = characters.length; i < length; ++i) {
+        passwordText += characters.charAt(Math.floor(Math.random() * n));
+      }
+    }
 
-} else if (uppercaseCheck && numberCheck) {
-  characters += uppercase + number;
+    console.log(passwordText);
 
-} else if (numberCheck && specialCheck) {
-  characters += number + specialChar;
-
-} else if (uppercaseCheck && specialCheck) {
-  characters += uppercase + specialChar;
-
-} else if (uppercaseCheck) {
-  characters += uppercase;
-
-} else if (numberCheck) {
-  characters += number;
-
-} else if (specialCheck) {
-  characters += specialChar;
-
-} else {
-  characters === lowercase;
-}
-
-for (var i = 0; i < passwordLength; i++) {
-  password += characters.charAt(Math.floor(Math.random() * characters.length));
-}
-return password;
-
-function writePassword() {
-  var passLength = prompt("How many characters out of 8-128 do you want your password to be?");
-  var length = passLength
-  characters = "`~!@#$%^&*()_+\*-=[];'/.,<>?:ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890",
-    passwordText = "";
-
-
-  for (var i = 0, n = characters.length; i < length; ++i) {
-    passwordText += characters.charAt(Math.floor(Math.random() * n));
-  }
-}
-
-console.log(passwordText);
-
-var generatedPassword = document.querySelector("#password");
+    var generatedPassword = document.querySelector("#password");
 
 
 
-function resetText() {
-  document.getElementById("password").value = "Your Secure Password";
-}
+    function resetText() {
+      document.getElementById("password").value = "Your Secure Password";
+    }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword)}
+    // Add event listener to generate button
+    generateBtn.addEventListener("click", writePassword)}}
